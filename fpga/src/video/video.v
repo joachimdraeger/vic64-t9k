68fe8280a3fdaa3a70d9d732d0148bd4f72cbd3c
Joachim Draeger <joachimdraeger+os@gmail.com>
2025-06-10 08:55:00 +0100

    Changes from https://github.com/vossstef/tang_nano_20k_c64 b2c30eeca47cc268eba64be501f55fcc83eb075b

--- fpga/src/video/video.v
+++ fpga/src/video/video.v
@@ -1,50 +1,73 @@
 // video.v
 
 module video (
-	      input	   clk_pixel,
-	      input	   por,
+          input	   clk,
+          input    clk_pixel_x5,
+          input    pll_lock,
+          input [8:0] audio_div,
 
+          input    ntscmode,
 	      input	   vs_in_n,
 	      input	   hs_in_n,
-	      input	   de_in,
+
 	      input [3:0]  r_in,
 	      input [3:0]  g_in,
 	      input [3:0]  b_in,
 
-          input [15:0] audio_l,
-          input [15:0] audio_r,
+          input [17:0] audio_l,
+          input [17:0] audio_r,
+
+          output osd_status,
 
           // (spi) interface from MCU
-          input	       mcu_start,
-          input	       mcu_osd_strobe,
+          input	   mcu_start,
+          input	   mcu_osd_strobe,
           input [7:0]  mcu_data,
-
-          output       vreset,
-          output [1:0] vmode,
 
           // values that can be configure by the user via osd          
           input [1:0]  system_scanlines,
+          input [1:0]  system_volume,
+          input	       system_wide_screen,
 
-          // digital video out for lcd
-          output lcd_clk,
-          output lcd_hs_n,
-          output lcd_vs_n,
-          output lcd_de,
-          output [5:0] lcd_r,
-          output [5:0] lcd_g,
-          output [5:0] lcd_b
-);
+	      // hdmi/tdms
+	      output	   tmds_clk_n,
+	      output	   tmds_clk_p,
+	      output [2:0] tmds_d_n,
+	      output [2:0] tmds_d_p  
+	      );
    
+
+
+
 /* -------------------- HDMI video and audio -------------------- */
 
+// generate 48khz audio clock
+reg clk_audio;
+
+reg [8:0] aclk_cnt;
+reg vresetD;
+
+always @(posedge clk) begin
+    // divisor = pixel clock / 48000 / 2 - 1
+    if(aclk_cnt < audio_div)
+        aclk_cnt <= aclk_cnt + 9'd1;
+    else begin
+        aclk_cnt <= 9'd0;
+        clk_audio <= ~clk_audio;
+    end
+end
+
+wire vreset;
+wire [1:0] vmode;
+
 video_analyzer video_analyzer (
-   .clk(clk_pixel),
+   .clk(clk),
    .vs(vs_in_n),
    .hs(hs_in_n),
-   .de(de_in),
-
+   .de(1'b1),
+   .ntscmode(ntscmode),
    .mode(vmode),
-   .vreset(vreset)  // reset signal
+   .vreset(vreset)
 );  
 
 wire sd_hs_n, sd_vs_n; 
@@ -52,10 +75,10 @@ wire [5:0] sd_r;
 wire [5:0] sd_g;
 wire [5:0] sd_b;
   
-scandoubler #(10) scandoubler (
+scandoubler #(11) scandoubler (
         // system interface
-        .clk_sys(clk_pixel),
-        .bypass(vmode == 2'd2),      // bypass in ST high/mono
+        .clk_sys(clk),
+        .bypass(1'b0),
         .ce_divider(1'b1),
         .pixel_ena(),
 
@@ -77,9 +100,13 @@ scandoubler #(10) scandoubler (
         .b_out(sd_b)
 );
 
+wire [5:0] osd_r;
+wire [5:0] osd_g;
+wire [5:0] osd_b;  
+
 osd_u8g2 osd_u8g2 (
-        .clk(clk_pixel),
-        .reset(por),
+        .clk(clk),
+        .reset(!pll_lock),
 
         .data_in_strobe(mcu_osd_strobe),
         .data_in_start(mcu_start),
@@ -92,55 +119,66 @@ osd_u8g2 osd_u8g2 (
         .g_in(sd_g),
         .b_in(sd_b),
 		     
-        .r_out(lcd_r),
-        .g_out(lcd_g),
-        .b_out(lcd_b)
+        .r_out(osd_r),
+        .g_out(osd_g),
+        .b_out(osd_b),
+        .osd_status(osd_status)
 );   
 
-assign lcd_clk = clk_pixel;
-assign lcd_hs_n = sd_hs_n;
-assign lcd_vs_n = sd_vs_n;
+wire [2:0] tmds;
+wire tmds_clock;
 
-reg [9:0] hcnt;   // max 1023
-reg [9:0] vcnt;   // max 626
+// Audio c64 core specific
+reg [15:0] alo,aro;
+always @(posedge clk) begin
+	reg [16:0] alm,arm;
 
-// generate lcd de signal
-// this needs adjustment for each ST video mode
-localparam XNTSC = 10'd920;
-localparam YNTSC = 10'd990;
-localparam XPAL  = 10'd920;
-localparam YPAL  = 10'd930;
-localparam XHIGH = 10'd955;
-localparam YHIGH = 10'd0;
-
-assign lcd_de = (hcnt < 10'd800) && (vcnt < 10'd480);
-
-// after scandoubler (with dim lines), ste video is 3*6 bits
-// lcd r and b are only 5 bits, so there may be some color
-// offset
-   
-always @(posedge clk_pixel) begin
-   reg 	     last_vs_n, last_hs_n;
-
-   last_hs_n <= lcd_hs_n;   
-
-   // rising edge/end of hsync
-   if(lcd_hs_n && !last_hs_n) begin
-      hcnt <= 
-        (vmode == 2'd0)?XNTSC:
-        (vmode == 2'd1)?XPAL:
-                        XHIGH;
-      
-      last_vs_n <= lcd_vs_n;   
-      if(lcd_vs_n && !last_vs_n) begin
-        vcnt <=
-        (vmode == 2'd0)?YNTSC:
-        (vmode == 2'd1)?YPAL:
-                        YHIGH;
-      end else
-	vcnt <= vcnt + 10'd1;    
-   end else
-      hcnt <= hcnt + 10'd1;    
+	arm <= {audio_r[17],audio_r[17:2]};
+	alm <= {audio_l[17],audio_l[17:2]};
+	alo <= ^alm[16:15] ? {alm[16], {15{alm[15]}}} : alm[15:0];
+	aro <= ^arm[16:15] ? {arm[16], {15{arm[15]}}} : arm[15:0];
 end
+
+// scale audio for valume by signed division
+wire [15:0] audio_vol_l = 
+    (system_volume == 2'd0)?16'd0:
+    (system_volume == 2'd1)?{ {2{alo[15]}}, alo[15:2] }:
+    (system_volume == 2'd2)?{ alo[15], alo[15:1] }:
+    alo;
+
+wire [15:0] audio_vol_r = 
+    (system_volume == 2'd0)?16'd0:
+    (system_volume == 2'd1)?{ {2{aro[15]}}, aro[15:2] }:
+    (system_volume == 2'd2)?{ aro[15], aro[15:1] }:
+    aro;
+
+hdmi #(
+   .AUDIO_RATE(48000), 
+   .AUDIO_BIT_WIDTH(16),
+   .VENDOR_NAME( { "MiSTle", 16'd0} ),
+   .PRODUCT_DESCRIPTION( {"C64", 64'd0} )
+) hdmi(
+  .clk_pixel_x5(clk_pixel_x5),
+  .clk_pixel(clk),
+  .clk_audio(clk_audio),
+  .audio_sample_word( { audio_vol_l, audio_vol_r } ),
+  .tmds(tmds),
+  .tmds_clock(tmds_clock),
+
+  // video input
+  .stmode(vmode),    // current video mode PAL/NTSC/MONO
+  .wide(system_wide_screen),       // adopt to wide screen video
+  .reset(vreset),    // signal to synchronize HDMI
+  // Atari STE outputs 4 bits per color. Scandoubler outputs 6 bits (to be
+  // able to implement dark scanlines) and HDMI expects 8 bits per color
+  .rgb( { osd_r, 2'b00, osd_g, 2'b00, osd_b, 2'b00 } )
+);
+
+// differential output
+ELVDS_OBUF tmds_bufds [3:0] (
+        .I({tmds_clock, tmds}),
+        .O({tmds_clk_p, tmds_d_p}),
+        .OB({tmds_clk_n, tmds_d_n})
+);
 
 endmodule
--- fpga/src/video/video_analyzer.v
+++ fpga/src/video/video_analyzer.v
@@ -11,8 +11,9 @@ module video_analyzer
  input		  hs,
  input		  vs,
  input		  de,
+ input		  ntscmode,
 
- output reg [1:0] mode, // 0=ntsc, 1=pal, 2=mono
+ output reg[1:0] mode, // 0=ntsc, 1=pal, 2=mono
  output reg	  vreset
 );
    
@@ -20,8 +21,8 @@ module video_analyzer
 // generate a reset signal in the upper left corner of active video used
 // to synchonize the HDMI video generation to the Atari ST
 reg vsD, hsD;
-reg [12:0] hcnt;    // signal ranges 0..2047
-reg [12:0] hcntL;
+reg [13:0] hcnt;    // signal ranges 0..2047
+reg [13:0] hcntL;
 reg [9:0] vcnt;    // signal ranges 0..313
 reg [9:0] vcntL;
 reg changed;
@@ -29,6 +30,7 @@ reg changed;
 always @(posedge clk) begin
     // ---- hsync processing -----
     hsD <= hs;
+    mode <= {1'b0 , ~ntscmode}; // 0=ntsc, 1=pal, 2=mono
 
     // begin of hsync, falling edge
     if(!hs && hsD) begin
@@ -39,7 +41,7 @@ always @(posedge clk) begin
 
         hcnt <= 0;
     end else
-        hcnt <= hcnt + 13'd1;
+        hcnt <= hcnt + 14'd1;
 
     if(!hs && hsD) begin
        // ---- vsync processing -----
@@ -48,36 +50,21 @@ always @(posedge clk) begin
        if(!vs && vsD) begin
           // check if image height has changed during last cycle
           vcntL <= vcnt;
-          if(vcntL != vcnt)
-             changed <= 1'b1;
-
-          vcnt <= 0;
-	  
-	  // check for PAL/NTSC values
-	  if(vcnt == 10'd312 && hcntL == 13'd2047) mode <= 2'd1;  // PAL
-	  if(vcnt == 10'd262 && hcntL == 13'd2031) mode <= 2'd0;  // NTSC
-
-	  // check for MONO
-	  if(vcnt == 10'd500 && hcntL == 13'd895)  mode <= 2'd2;  // MONO
-	  
+          if(vcntL != vcnt) changed <= 1'b1;
+          vcnt <= 0;  
        end else
          vcnt <= vcnt + 10'd1;
     end
 
    // the reset signal is sent to the HDMI generator. On reset the
-   // HDMI re-adjusts its counters to the start of the visible screen
-   // area
-   
+   // HDMI re-adjusts its counters to the start of the visible screen area
    vreset <= 1'b0;
-   // account for back porches to adjust image position within the
-   // HDMI frame
-   if( (hcnt == 244 && vcnt == 36 && changed && mode == 2'd2) ||
-       (hcnt == 152 && vcnt == 28 && changed && mode == 2'd1) ||
-       (hcnt == 152 && vcnt == 18 && changed && mode == 2'd0) ) begin
-      vreset <= 1'b1;
-      changed <= 1'b0;
+   if( 
+       (hcnt == 1 && vcnt == 20 && changed && mode == 2'd1) ||
+       (hcnt == 1 && vcnt == 10 && changed && mode == 2'd0) ) begin
+       vreset <= 1'b1;
+       changed <= 1'b0;
    end
 end
-
 
 endmodule
